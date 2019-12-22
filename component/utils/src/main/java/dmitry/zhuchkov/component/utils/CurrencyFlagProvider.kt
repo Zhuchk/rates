a87378801d831222b501100f388045271d42150b
Dmitrii Zhuchkov <=>
2019-12-23 00:05:01 +0300

    resources for flags

--- component/utils/src/main/java/dmitry/zhuchkov/component/utils/CurrencyFlagProvider.kt
+++ component/utils/src/main/java/dmitry/zhuchkov/component/utils/CurrencyFlagProvider.kt
@@ -8,10 +8,10 @@ object CurrencyFlagProvider {
 
 	fun getFlagByCode(code: String): String =
 		when (code) {
-			"EUR" -> "https://github.com/Zhuchk/rates/blob/master/united-states.png?raw=true"
-			"USD" -> "https://github.com/Zhuchk/rates/blob/master/united-states.png"
-			"RUB" -> "https://github.com/Zhuchk/rates/blob/master/united-states.png"
+			"EUR" -> "https://github.com/Zhuchk/rates/blob/master/flags/european-union.png?raw=true"
+			"USD" -> "https://github.com/Zhuchk/rates/blob/master/flags/united-states.png?raw=true"
+			"RUB" -> "https://github.com/Zhuchk/rates/blob/master/flags/russia.png?raw=true"
 
-			else  -> "https://github.com/Zhuchk/rates/blob/master/united-states.png"
+			else  -> "https://github.com/Zhuchk/rates/blob/master/flags/coin.png?raw=true"
 		}
 }
